28f84d9487ac88c13495d56db12ffe4452e5b350
Viggo Lagerstedt Ekholm <viggo.lagerstedtekholm@afry.com>
2022-02-21 00:03:39 +0100

    Design.

--- src/Components/Profile/Degrees/CourseInDegree.tsx
+++ src/Components/Profile/Degrees/CourseInDegree.tsx
@@ -1,16 +1,65 @@
-import { Card } from "react-bootstrap";
+import { Card, Col, Container, Row, Image, Button } from "react-bootstrap";
 import { ICourse } from "../../Service/DegreeService";
+import {useNavigate} from "react-router-dom";
+
+// @ts-ignore
+import CourseDefault from '../../../Images/CourseDefault.png';
 
 interface Props {
     course: ICourse;
 }
 
 function CourseInDegree({ course }: Props) {
+    const navigate = useNavigate();
+
     return (
-        <Card className="bg-secondary bg-opacity-10 my-2">
+        <Card className="bg-secondary bg-opacity-10 mx-2 my-2">
             <Card.Body>
-                <Card.Title>{course.name}</Card.Title>
+                <Container>
+            
+                    <Row>
+                        <h4 className="text-center">{course.name}</h4>
+                    </Row>
+
+                    <Row>
+                        <Button onClick={() => navigate('/course/' + course.id)} className="mx-2 my-2">
+                            Go to course page
+                        </Button>
+                    </Row>
+
+                    <hr />
+
+                    <Row>
+                        <Col>
+                            <p><b>University</b></p>
+                            <p>{course.university}</p>
+                        </Col>
+
+                        <Col>
+                            <p><b>City</b></p>
+                            <p>{course.city}</p>
+                        </Col>
+
+                        <Col>
+                            <p><b>Country</b></p>
+                            <p>{course.country}</p>
+                        </Col>
+
+                        <Col>
+                            <p><b>Code</b></p>
+                            <p>{course.code}</p>
+                        </Col>
+
+                        <Col>
+                            <p><b>Credits</b></p>
+                            <p>{course.credits.toString()}</p>
+                        </Col>
+                    </Row>
+                </Container>
             </Card.Body>
+            <Card.Footer>
+                <p>{course.added.toString()}</p>
+            </Card.Footer>
         </Card>
     )
 }
